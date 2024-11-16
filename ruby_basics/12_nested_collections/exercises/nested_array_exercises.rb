83451cd5f3ca87275aab14f51ed386bb62d17f7a
Kristupas <108592864+removeris@users.noreply.github.com>
2024-11-16 23:11:06 +0200

    Complete nested collections exercices.

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -9,34 +9,56 @@ def blank_seating_chart(number_of_rows, seats_per_row)
   #   [nil, nil, nil]
   # ]
 
+  array = Array.new(number_of_rows) {Array.new(seats_per_row)}
+
+  return array;
+
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
+  chart[row_index].push(seat_to_add);
+  return chart
   # at row_index index of the chart, then return the chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
+  chart.push(row_to_add)
+  return chart
   # then return the chart.
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
-
+  chart[row_index].delete_at(seat_index)
+  return chart
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
+  chart.delete_at(row_index)
+  return chart
   # then return the chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
+  count = 0
+
+  chart.each do | row |
+    row.each do | element|
+      if element == nil
+        count += 1
+      end
+    end
+  end
+
+  return count
 
   # NOTE: `chart` should **not** be mutated
 end
@@ -53,6 +75,13 @@ def find_favorite(array_of_hash_objects)
   #   { name: 'HTML', is_my_favorite?: false }
   # ]
 
+  array_of_hash_objects.each do | hash |
+    if hash[:is_my_favorite?] == true
+      return hash
+    end
+  end
+
+  return nil
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
 end
